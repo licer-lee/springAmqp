4a30ea9ac326978e5b23d0be406a3c89bf6e4a3f
licerlee <licerlee@gmail.com>
2019-10-22 17:15:59 +0800

    消费者修改

--- src/main/java/com/licerlee/redis/web/ExamTask.java
+++ src/main/java/com/licerlee/redis/web/ExamTask.java
@@ -14,9 +14,9 @@ public class ExamTask {
 
 
     @RabbitHandler
-//    public void receiveAndGeneratePaper(Object message){
-    public void receiveAndGeneratePaper(Message message){
-
+    public void receiveAndGeneratePaper(Object message){
+//    public void receiveAndGeneratePaper(Message message){
+        // TODO 使用Object以外的类型会报错，具体原因待调查
         if(message instanceof Message){
             message = (Message) message;
             log.warn("msg type {}", message.getClass());
